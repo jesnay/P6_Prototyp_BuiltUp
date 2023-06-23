4b2e3bc3150a18dca45f15203aac523d9091cfbe
Max <max.m.mittelstaedt@stud.h-da.de>
2023-06-23 15:29:16 +0200

    cameraoffset script

--- Assets/Scenes/P6 Unity/Scripts/OffGameSettings.cs
+++ Assets/Scenes/P6 Unity/Scripts/OffGameSettings.cs
@@ -7,10 +7,12 @@ using UnityEngine.SceneManagement;
 
 public class OffGameSettings : MonoBehaviour
 {
+    private GameObject cameraOffset;
     // Start is called before the first frame update
     void Start()
     {
-
+        cameraOffset = GameObject.Find("CameraOffset");
+        cameraOffset.transform.position = new Vector3(0f, 1.65f, 0.5f);
     }
 
     // Update is called once per frame
